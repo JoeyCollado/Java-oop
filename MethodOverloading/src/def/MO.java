7abf1fd15418a5491cbd48dd3683ca85187de5f8
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-08-23 13:54:54 +0800

    method overloading

---
+++ MethodOverloading/src/def/MO.java
@@ -0,0 +1,21 @@
+package def;
+
+import def.User;
+
+public class MO {
+
+	public static void main(String[] args) {
+		//getters and setters = method from encapsulation that allow us access variables from outside class
+		
+		
+		 User user = new User();
+		 user.set_name("Joey"); //set value
+		 user.membership = "Gold";
+		 
+		 System.out.println(user.get_name()); //get value
+	
+		 
+		 
+	}
+
+}
