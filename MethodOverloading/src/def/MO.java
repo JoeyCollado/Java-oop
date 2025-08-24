46b8db8c0ff6fdd491f8ff8a84aba9c8ce5380a5
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-08-24 12:36:24 +0800

    method overloading done

--- MethodOverloading/src/def/MO.java
+++ MethodOverloading/src/def/MO.java
@@ -5,14 +5,18 @@ import def.User;
 public class MO {
 
 	public static void main(String[] args) {
-		//getters and setters = method from encapsulation that allow us access variables from outside class
+		//method overloading = allow us to create different variations of methods with different types
 		
 		
 		 User user = new User();
 		 user.set_name("Joey"); //set value
-		 user.membership = "Gold";
+		 user.set_membership("Gold");
+		 //invoke using enum variation
+		 user.set_membership(User.Membership.Silver);
 		 
 		 System.out.println(user.get_name()); //get value
+		 System.out.println(user.get_membership());	
+		 System.out.println(user.get_membership());
 	
 		 
 		 
--- MethodOverloading/src/def/User.java
+++ MethodOverloading/src/def/User.java
@@ -2,21 +2,36 @@ package def;
 
 public class User {
    String _name;
-   String membership;
+   private String _membership;
 
+   //1
    void set_name(String name) { //set method, give value
 	   _name = name;
 	   System.out.println("we can customize set values as extra processing");
    }
    
- 
    String get_name() { //get method, get value
    	return _name;
    }
+   //2
+   void set_membership(String membership) {
+	   _membership = membership; //setter
+	   
+   }
+   
+   //method overload
+   void set_membership(Membership membership) { //type, variableName, the type we are using is enum
+	   //converting enum value
+	   _membership = membership.name();
+   }
+   
+   public enum Membership {
+	   Bronze, Silver, Gold;
+   }
+
+   
+   String get_membership() { //getter
+	   return _membership;
+   }
 }
 
-//this allow us to determine what we can allow to enter or exit
-
-
-//either you can use this. or _ as a prefix to differentiate terms with same name
-//private = using this prefix forbid us from accessing that attribute from outside class
